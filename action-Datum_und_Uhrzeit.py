8de81e59a11af17f91139c5d57f656fd790c8e70
Amjad Saadeh <mail@amjadsaadeh.de>
2019-05-09 22:22:38 +0200

    add user credentials for mqtt access

--- action-Datum_und_Uhrzeit.py
+++ action-Datum_und_Uhrzeit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from hermes_python.hermes import Hermes
 import datetime
+from hermes_python.hermes import Hermes, MqttOptions
+import toml
 
 
 USERNAME_INTENTS = "domi"
@@ -67,5 +68,12 @@ def subscribe_intent_callback(hermes, intent_message):
 
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
+    config = toml.load('/etc/snips.toml')
+
+    broker_address = config['snips-common']['mqtt']
+    mqtt_username = config['snips-common']['mqtt_username']
+    mqtt_password = config['snips-common']['mqtt_password']
+
+    mqtt_opts = MqttOptions(username=mqtt_username, password=mqtt_password, broker_address=broker_address)
+    with Hermes(mqtt_options=mqtt_opts) as h:
         h.subscribe_intents(subscribe_intent_callback).start()
